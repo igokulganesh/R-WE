6470786d662ac9cc8dcdee3f96feefe1c3628efa
igokulganesh <47038733+igokulganesh@users.noreply.github.com>
2020-05-26 23:32:23 +0530

    Update signup.js

--- signup.js
+++ signup.js
@@ -1,5 +1,17 @@
-
-
+  firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // [START_EXCLUDE]
+    if (errorCode == 'auth/weak-password') {
+      alert('The password is too weak.');
+    } else {
+      alert(errorMessage);
+    }
+    console.log(error);
+    // [END_EXCLUDE]
+  });
+  
   
 const auth=firebase.auth();
 
@@ -7,11 +19,13 @@ const auth=firebase.auth();
 function signUp(){
 	
 		
-		var email = document.getElementById("email");
-		var password = document.getElementById("psw");
+		var email = document.getElementById("email_field");
+		var password = document.getElementById("password_field");
 		
 		const promise = auth.createUserWithEmailAndPassword(email.value, password.value);
 		promise.catch(e => alert(e.message));
 		
 		alert("Signed Up Succcesfully");
 	}
+
+
